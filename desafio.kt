0c936459ba05cad29278680bf78c15c6c8998ad2
Guilherme Antonio Silva <102996571+GU1LHERMESILV4@users.noreply.github.com>
2023-10-14 21:13:39 -0300

    Ajustes de Funções e Classes

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,37 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String, var formacao: Formacao? = null)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        usuario.formacao = this
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Leo Stronda")
+    val usuario2 = Usuario("Júlio Balestrin")
+    val usuario3 = Usuario("Fábio Giga")
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 120)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 180)
+    val conteudo3 = ConteudoEducacional("Banco de Dados Relacional", 240)
+
+    val formacao1 = Formacao("Desenvolvimento de Software", listOf(conteudo1, conteudo2, conteudo3))
+    val formacao2 = Formacao("Análise de Dados", listOf(conteudo3, conteudo1))
+    val formacao3 = Formacao("Desenvolvimento Web", listOf(conteudo2, conteudo3))
+
+    formacao1.matricular(usuario1)
+    formacao2.matricular(usuario2)
+    formacao3.matricular(usuario3)
+
+    println("${usuario1.nome}, Matriculado em ${usuario1.formacao?.nome}")
+    println("${usuario2.nome}, Matriculado em ${usuario2.formacao?.nome}")
+    println("${usuario3.nome}, Matriculado em ${usuario3.formacao?.nome}")
 }
